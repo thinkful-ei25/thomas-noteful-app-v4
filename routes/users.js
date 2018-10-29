9e8560172ce9d83d11ccbd468c21983ccf950e03
Thomas Chun <tchun2055@gmail.com>
2018-10-29 12:56:42 -0700

    added bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,28 @@ const router = express.Router();
 /* ========== POST (/api/users) USER ENDPOINT ========== */
 router.post('/', (req, res, next) => {
   const { fullname, username, password } = req.body;
-  const newUser = { fullname, username, password };
 
-  User.create(newUser)
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+        fullname
+      };
+      return User.create(newUser);
+    })
     .then(result => {
-      res.location(`${req.originalUrl}/${result.id}`)
+      res.location(`/api/users/${result.id}`)
         .status(201)
         .json(result);
     })
-    .catch(err => next(err));
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
 });
 
 module.exports = router;
